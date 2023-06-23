447c3a77c84867e16bfff1e761d6e00b04f8bc22
blair43 <blair.y.inmenzo@gmail.com>
2023-06-23 14:54:47 +0800

    google sample test

---
+++ Test Suites/googlesimpletest.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>googlesimpletest</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>2528256b-9e07-4cde-aba7-26826d23e50c</testSuiteGuid>
+   <testCaseLink>
+      <guid>9d0949d6-2e49-4151-9fc9-f96a521453f6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/google</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
