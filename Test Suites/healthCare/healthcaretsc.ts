a10db06b354a33a888d6ca6d3d37e4cb048f4e76
blair43 <blair.y.inmenzo@gmail.com>
2023-07-12 15:51:25 +0800

    test

--- Test Suites/healthCare/healthcaretsc.ts
+++ Test Suites/healthCare/healthcaretsc.ts
@@ -4,7 +4,7 @@
    <name>healthcaretsc</name>
    <tag></tag>
    <delayBetweenInstances>0</delayBetweenInstances>
-   <executionMode>PARALLEL</executionMode>
+   <executionMode>SEQUENTIAL</executionMode>
    <maxConcurrentInstances>5</maxConcurrentInstances>
    <testSuiteRunConfigurations>
       <TestSuiteRunConfiguration>
